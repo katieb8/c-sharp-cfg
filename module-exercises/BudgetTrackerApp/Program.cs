3dfbaebc55e97ef1e3aa402221219744ec6cf0af
Katie Burdett <katie.burdett@ao.com>
2025-09-02 14:34:57 +0100

    chore: using lists

--- module-exercises/BudgetTrackerApp/Program.cs
+++ module-exercises/BudgetTrackerApp/Program.cs
@@ -7,8 +7,14 @@ double entertainmentBudget = 250.00;
 Console.WriteLine($"Grocery budget: {groceryBudget}");
 Console.WriteLine($"Entertainment budget: {entertainmentBudget}");
 
-double[] fixedExpenses = { 1200.00, 150.00, 50.00 };
+List<double> fixedExpenses = new List<double> { 1200.00, 150.00, 50.00 };
+// double[] fixedExpenses = { 1200.00, 150.00, 50.00 };
 Console.WriteLine("Fixed expenses: ");
 Console.WriteLine("Rent: " + fixedExpenses[0]);
 Console.WriteLine("Electricity: " + fixedExpenses[1]);
 Console.WriteLine("Water: " + fixedExpenses[2]);
+
+fixedExpenses.Add(35);
+fixedExpenses.Add(9.99);
+Console.WriteLine("Internet: " + fixedExpenses[3]);
+Console.WriteLine("Spotify: " + fixedExpenses[4]);
