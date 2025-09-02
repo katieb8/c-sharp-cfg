68fc99771a4ecbeae60999d15be209f85052a60b
Katie Burdett <katie.burdett@ao.com>
2025-09-02 14:30:00 +0100

    chore: initial commit

---
+++ module-exercises/BudgetTrackerApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to our Budget Tracker!");
+
+double groceryBudget = 200.00;
+double entertainmentBudget = 250.00;
+
+Console.WriteLine($"Grocery budget: {groceryBudget}");
+Console.WriteLine($"Entertainment budget: {entertainmentBudget}");
+
+double[] fixedExpenses = { 1200.00, 150.00, 50.00 };
+Console.WriteLine("Fixed expenses: ");
+Console.WriteLine("Rent: " + fixedExpenses[0]);
+Console.WriteLine("Electricity: " + fixedExpenses[1]);
+Console.WriteLine("Water: " + fixedExpenses[2]);
